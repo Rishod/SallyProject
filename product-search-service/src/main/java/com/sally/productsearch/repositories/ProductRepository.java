fb2b2a84a49123df339e0d686c6d0e0df8140820
Vlad Orshulyak <vladorshulyak@gmail.com>
2021-09-06 18:03:45 +0300

    make elasticsearch works

--- product-search-service/src/main/java/com/sally/productsearch/repositories/ProductRepository.java
+++ product-search-service/src/main/java/com/sally/productsearch/repositories/ProductRepository.java
@@ -4,12 +4,9 @@ import com.sally.productsearch.entities.ProductEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource(path = "products")
+@Repository
 public interface ProductRepository extends ElasticsearchRepository<ProductEntity, String> {
-
-    @RestResource(path = "find")
     Page<ProductEntity> findByTitle(String title, Pageable pageable);
 }
