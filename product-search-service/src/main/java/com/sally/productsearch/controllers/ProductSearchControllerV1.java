320e1e2dfa1700eb605219d93a8e43869f3a50f8
Vlad Orshulyak <vladorshulyak@gmail.com>
2021-09-06 18:03:45 +0300

    make elasticsearch works

--- product-search-service/src/main/java/com/sally/productsearch/controllers/ProductSearchControllerV1.java
+++ product-search-service/src/main/java/com/sally/productsearch/controllers/ProductSearchControllerV1.java
@@ -1,8 +1,26 @@
 package com.sally.productsearch.controllers;
 
+import com.sally.productsearch.entities.ProductEntity;
+import com.sally.productsearch.repositories.ProductRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductSearchControllerV1 {
+
+    private final ProductRepository productRepository;
+
+    public ProductSearchControllerV1(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/products")
+    public List<ProductEntity> getProducts(@RequestParam("title") String title) {
+        return productRepository.findByTitle(title, Pageable.unpaged()).getContent();
+    }
 
 }
